2421cb18abebdd8fbbcb9c058240b0ee297c5c20
Slava Telyatko <energokip2017@gmail.com>
2020-07-04 19:38:57 +0300

    03-04 done

--- task/03-date-tasks.js
+++ task/03-date-tasks.js
@@ -22,7 +22,7 @@
  *    'Sun, 17 May 1998 03:00:00 GMT+01' => Date()
  */
 function parseDataFromRfc2822(value) {
-   throw new Error('Not implemented');
+   return new Date(value);
 }
 
 /**
@@ -37,7 +37,7 @@ function parseDataFromRfc2822(value) {
  *    '2016-01-19T08:07:37Z' => Date()
  */
 function parseDataFromIso8601(value) {
-   throw new Error('Not implemented');
+   return new Date(value);
 }
 
 
@@ -56,7 +56,8 @@ function parseDataFromIso8601(value) {
  *    Date(2015,1,1)    => false
  */
 function isLeapYear(date) {
-   throw new Error('Not implemented');
+   const year = date.getFullYear();
+   return year % 4 === 0 && (year % 400 === 0 || year % 100 !== 0)
 }
 
 
@@ -77,6 +78,7 @@ function isLeapYear(date) {
  */
 function timeSpanToString(startDate, endDate) {
    throw new Error('Not implemented');
+   // return new Date(endDate - startDate).toISOString().slice(11, -1)
 }
 
 
@@ -94,7 +96,8 @@ function timeSpanToString(startDate, endDate) {
  *    Date.UTC(2016,3,5,21, 0) => Math.PI/2
  */
 function angleBetweenClockHands(date) {
-    throw new Error('Not implemented');
+    const rad = Math.abs((date.getUTCHours() * 60  - date.getUTCMinutes() * 11) / 2) % 360;
+    return (rad <= 180 ? rad : 360 - rad) / 180 * Math.PI
 }
 
 
